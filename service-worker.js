cdb30e6c9729813e8528481684eed5e6082b510a
dublinblue70 <dave.s.carter@gmail.com>
2026-09-16 18:01:42 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "parknacross-weather-v38-4-11-sea-temp-labels-20260916";
+const CACHE_NAME = "parknacross-weather-v38-4-14-warning-badges-20260916";
 const STATIC_ASSETS = [
   "./",
   "./index.html",
