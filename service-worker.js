0ae66e035545047be725d1621cf5b280e4bfd96c
dublinblue70 <dave.s.carter@gmail.com>
2026-09-24 15:02:33 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "parknacross-v38-4-87-consolidated-weather-pages";
+const CACHE_NAME = "parknacross-v38-4-88-lightning-alert-range";
 const STATIC_ASSETS = [
   "./",
   "./index.html",
